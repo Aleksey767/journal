2c0b0e4c047c35fac2798e167d73b8a3b1c43a87
al_sazonov@list.ru <al_sazonov@list.ru>
2024-09-26 16:44:45 +0200

    fix build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { JournalComponent } from "./components/journal/journal.component";
-import { PairTableComponent } from "./components/pair-table/pair-table.component"; 
+import { PairTableComponent } from "./components/pair-table/pair-table.component";
 import { CommonModule } from '@angular/common';
-import {GradesComponent} from "./components/grades/grades.component";
+import { GradesComponent } from "./components/grades/grades.component";
+import { ActivityComponent } from './components/activity/activity.component';
+import { ReportComponent } from './components/report/report.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, JournalComponent, PairTableComponent, CommonModule,GradesComponent, GradesComponent],
+  imports: [RouterOutlet, JournalComponent, PairTableComponent, CommonModule, GradesComponent, GradesComponent, ReportComponent, ActivityComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'] 
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   pairs: { teamA: string; teamB: string }[] = [];
